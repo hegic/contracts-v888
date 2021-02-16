194dffa4ab51a2f3bc2c971d01d6709a2b5debd0
George Ornbo <george@shapeshed.com>
2021-02-16 11:53:00 +0000

    Add workflow files and README
    - Fixes migration issue where ETHOptions was incorrectly initialised
    - Adds Github Action workflows for Testing and Code Coverage
    - Adds basic README
    - Add Issue and Pull Request Templates
    - Adds an nvmrc file for users of nvm (https://github.com/nvm-sh/nvm)

---
+++ README.md
@@ -0,0 +1,32 @@
+# Hegic v888
+
+![Continuous Integration](https://github.com/shapeshed/contracts-v888/workflows/Continuous%20Integration/badge.svg)
+[![codecov](https://codecov.io/gh/shapeshed/contracts-v888/branch/master/graph/badge.svg?token=hb3cd1MLwj)](https://codecov.io/gh/shapeshed/contracts-v888)
+
+## Prerequisities
+
+- [Node.js v12.20.2][1]
+
+## Install
+
+    npm install
+
+## Tasks
+
+Start ganache
+
+    npm run ganache
+
+Run tests
+
+    npm test
+
+Lint files
+
+    npm run lint
+
+Generate code coverage
+
+    npm run coverage
+
+[1]: https://nodejs.org/
--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -19,126 +19,157 @@ const BC = artifacts.require("LinearBondingCurve")
 const CONTRACTS_FILE = process.env.CONTRACTS_FILE
 
 const params = {
-    ETHPrice: new BN(380e8),
-    BTCPrice: new BN("1161000000000"),
-    ETHtoBTC(){return this.ETHPrice.mul(new BN("10000000000000000000000000000000")).div(this.BTCPrice)},
-    ExchangePrice: new BN(30e8),
-    BC:{
-        k: new BN("100830342800"),
-        startPrice: new BN("350000000000000")
-    }
+  ETHPrice: new BN(380e8),
+  BTCPrice: new BN("1161000000000"),
+  ETHtoBTC() {
+    return this.ETHPrice.mul(new BN("10000000000000000000000000000000")).div(
+      this.BTCPrice
+    )
+  },
+  ExchangePrice: new BN(30e8),
+  BC: {
+    k: new BN("100830342800"),
+    startPrice: new BN("350000000000000"),
+  },
 }
 
 module.exports = async function (deployer, network, [account]) {
-    if (["development", "develop", 'soliditycoverage'].indexOf(network) >= 0) {
-      const w = await deployer.deploy(WBTC)
-      const h = await deployer.deploy(HEGIC)
-      await deployer.deploy(ETHPool)
-      await deployer.deploy(BC, HEGIC.address, params.BC.k, params.BC.startPrice)
-      await deployer.deploy(Exchange, WBTC.address, params.ETHtoBTC())
-      await deployer.deploy(PriceProvider, params.ETHPrice)
-      await deployer.deploy(BTCPriceProvider, params.BTCPrice)
+  if (["development", "develop", "soliditycoverage"].indexOf(network) >= 0) {
+    const w = await deployer.deploy(WBTC)
+    const h = await deployer.deploy(HEGIC)
+    await deployer.deploy(ETHPool)
+    await deployer.deploy(BC, HEGIC.address, params.BC.k, params.BC.startPrice)
+    await deployer.deploy(Exchange, WBTC.address, params.ETHtoBTC())
+    await deployer.deploy(PriceProvider, params.ETHPrice)
+    await deployer.deploy(BTCPriceProvider, params.BTCPrice)
 
-      await deployer.deploy(StakingWBTC, HEGIC.address, WBTC.address)
-      await deployer.deploy(StakingETH, HEGIC.address)
+    await deployer.deploy(StakingWBTC, HEGIC.address, WBTC.address)
+    await deployer.deploy(StakingETH, HEGIC.address)
 
-      await deployer.deploy(ETHOptions,
-          PriceProvider.address,
-          StakingETH.address
+    await deployer.deploy(ETHOptions, PriceProvider.address, StakingETH.address, ETHPool.address)
+    await deployer.deploy(
+      WBTCOptions,
+      BTCPriceProvider.address,
+      Exchange.address,
+      WBTC.address,
+      StakingWBTC.address
+    )
+    const ETHPoolAddress = await ETHOptions.deployed().then((x) => x.pool())
+    const WBTCPoolAddress = await WBTCOptions.deployed().then((x) => x.pool())
+    const er = await deployer.deploy(
+      ETHRewards,
+      ETHOptions.address,
+      HEGIC.address
+    )
+    const wr = await deployer.deploy(
+      WBTCRewards,
+      WBTCOptions.address,
+      HEGIC.address
+    )
+    const esr = await deployer.deploy(
+      ETHStakingRewards,
+      account,
+      account,
+      HEGIC.address,
+      ETHPoolAddress
+    )
+    const wsr = await deployer.deploy(
+      WBTCStakingRewards,
+      account,
+      account,
+      HEGIC.address,
+      WBTCPoolAddress
+    )
+
+    await h.mintTo(BC.address, "753001000000000000000000000")
+    await h.mintTo(ETHRewards.address, "100000000000000000000000000")
+    await h.mintTo(WBTCRewards.address, "100000000000000000000000000")
+    await h.mintTo(
+      ETHStakingRewards.address,
+      "10000000000000000000000000000000"
+    )
+    await h.mintTo(
+      WBTCStakingRewards.address,
+      "10000000000000000000000000000000"
+    )
+    await esr.notifyRewardAmount("4620000000000000000000000")
+    await wsr.notifyRewardAmount("4620000000000000000000000")
+    await er.setRewardsRate(26229508196)
+    await wr.setRewardsRate("8213552361396304000000")
+    if (CONTRACTS_FILE) {
+      const fs = require("fs")
+      console.log("> Contracts writing: " + CONTRACTS_FILE)
+      fs.writeFileSync(
+        CONTRACTS_FILE,
+        JSON.stringify({
+          WBTC: {
+            address: WBTC.address,
+            abi: WBTC.abi,
+          },
+          ETHPriceProvider: {
+            address: PriceProvider.address,
+            abi: PriceProvider.abi,
+          },
+          BTCPriceProvider: {
+            address: BTCPriceProvider.address,
+            abi: BTCPriceProvider.abi,
+          },
+          WBTC: {
+            address: WBTC.address,
+            abi: WBTC.abi,
+          },
+          HEGIC: {
+            address: HEGIC.address,
+            abi: HEGIC.abi,
+          },
+          ETHOptions: {
+            address: ETHOptions.address,
+            abi: ETHOptions.abi,
+          },
+          WBTCOptions: {
+            address: WBTCOptions.address,
+            abi: WBTCOptions.abi,
+          },
+          ETHPool: {
+            address: await ETHOptions.deployed().then((x) => x.pool()),
+            abi: await ETHPool.abi,
+          },
+          WBTCPool: {
+            address: await WBTCOptions.deployed().then((x) => x.pool()),
+            abi: await ERCPool.abi,
+          },
+          ETHStaking: {
+            address: StakingETH.address,
+            abi: StakingETH.abi,
+          },
+          WBTCStaking: {
+            address: StakingWBTC.address,
+            abi: StakingWBTC.abi,
+          },
+          ETHRewards: {
+            address: ETHRewards.address,
+            abi: ETHRewards.abi,
+          },
+          WBTCRewards: {
+            address: WBTCRewards.address,
+            abi: WBTCRewards.abi,
+          },
+          BC: {
+            address: BC.address,
+            abi: BC.abi,
+          },
+          ETHStakingRewards: {
+            address: ETHStakingRewards.address,
+            abi: ETHStakingRewards.abi,
+          },
+          WBTCStakingRewards: {
+            address: WBTCStakingRewards.address,
+            abi: WBTCStakingRewards.abi,
+          },
+        })
       )
-      await deployer.deploy(WBTCOptions,
-          BTCPriceProvider.address,
-          Exchange.address,
-          WBTC.address,
-          StakingWBTC.address
-      )
-      const ETHPoolAddress = await ETHOptions.deployed().then(x => x.pool())
-      const WBTCPoolAddress = await WBTCOptions.deployed().then(x => x.pool())
-      const er = await deployer.deploy(ETHRewards, ETHOptions.address, HEGIC.address)
-      const wr = await deployer.deploy(WBTCRewards, WBTCOptions.address, HEGIC.address)
-      const esr = await deployer.deploy(ETHStakingRewards, account, account, HEGIC.address, ETHPoolAddress)
-      const wsr = await deployer.deploy(WBTCStakingRewards, account, account, HEGIC.address, WBTCPoolAddress)
-
-      await h.mintTo(BC.address, "753001000000000000000000000")
-      await h.mintTo(ETHRewards.address, "100000000000000000000000000")
-      await h.mintTo(WBTCRewards.address, "100000000000000000000000000")
-      await h.mintTo(ETHStakingRewards.address, "10000000000000000000000000000000")
-      await h.mintTo(WBTCStakingRewards.address, "10000000000000000000000000000000")
-      await esr.notifyRewardAmount('4620000000000000000000000')
-      await wsr.notifyRewardAmount('4620000000000000000000000')
-      await er.setRewardsRate(26229508196)
-      await wr.setRewardsRate('8213552361396304000000')
-      if(CONTRACTS_FILE){
-          const fs = require('fs');
-          console.log("> Contracts writing: " + CONTRACTS_FILE)
-          fs.writeFileSync(CONTRACTS_FILE, JSON.stringify({
-              WBTC: {
-                  address: WBTC.address,
-                  abi: WBTC.abi
-              },
-              ETHPriceProvider: {
-                  address: PriceProvider.address,
-                  abi: PriceProvider.abi
-              },
-              BTCPriceProvider: {
-                  address: BTCPriceProvider.address,
-                  abi: BTCPriceProvider.abi
-              },
-              WBTC: {
-                  address: WBTC.address,
-                  abi: WBTC.abi
-              },
-              HEGIC: {
-                  address: HEGIC.address,
-                  abi: HEGIC.abi
-              },
-              ETHOptions: {
-                  address: ETHOptions.address,
-                  abi: ETHOptions.abi
-              },
-              WBTCOptions: {
-                  address: WBTCOptions.address,
-                  abi: WBTCOptions.abi
-              },
-              ETHPool: {
-                  address: await ETHOptions.deployed().then(x => x.pool()),
-                  abi: await ETHPool.abi
-              },
-              WBTCPool: {
-                  address: await WBTCOptions.deployed().then(x => x.pool()),
-                  abi: await ERCPool.abi
-              },
-              ETHStaking: {
-                  address: StakingETH.address,
-                  abi: StakingETH.abi
-              },
-              WBTCStaking: {
-                  address: StakingWBTC.address,
-                  abi: StakingWBTC.abi
-              },
-              ETHRewards: {
-                  address: ETHRewards.address,
-                  abi: ETHRewards.abi
-              },
-              WBTCRewards: {
-                  address: WBTCRewards.address,
-                  abi: WBTCRewards.abi
-              },
-              BC:{
-                  address: BC.address,
-                  abi: BC.abi
-              },
-              ETHStakingRewards:{
-                  address: ETHStakingRewards.address,
-                  abi: ETHStakingRewards.abi
-              },
-              WBTCStakingRewards:{
-                  address: WBTCStakingRewards.address,
-                  abi: WBTCStakingRewards.abi
-              },
-          }))
-      }
+    }
   } else {
-      throw Error(`wrong network ${network}`)
+    throw Error(`wrong network ${network}`)
   }
 }
